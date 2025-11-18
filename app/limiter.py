5d6db03b72a3b82d0d1693d47ce87e473bc39051
Elvin cooper <ing.elvin01cooper@gmail.com>
2025-11-18 00:40:00 -0400

    fix: deshabilitando el limite en modo de pruebas

--- app/limiter.py
+++ app/limiter.py
@@ -1,8 +1,13 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask import current_app
 
 
 # configurar rate limiter
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"])
+    default_limits=["200 per day", "50 per hour"]
+    if not current_app.config.get("testing")
+    else ["0 per day"]  # Deshabilitar límites en modo testing
+    )
+

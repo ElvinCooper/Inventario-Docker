5f838ac7741432fb1f79b53858b71499a71be7a3
Elvin cooper <ing.elvin01cooper@gmail.com>
2025-11-18 01:04:34 -0400

    fix: deshabilitando el limite en modo de pruebas 5

--- app/limiter.py
+++ app/limiter.py
@@ -3,29 +3,29 @@ from flask_limiter.util import get_remote_address
 from flask import current_app
 
 
-# 1. Define la función que verifica la configuración.
-def get_dynamic_limits():
-    """Retorna límites normales o 0 si estamos en modo TESTING."""
-    # current_app solo se llama cuando esta función se ejecuta en contexto.
-    if current_app.config.get("testing", False):
-        return ["0 per day"] # Deshabilita límites
-    else:
-        return ["200 per day", "50 per hour"]
-
-
-# 2. Inicializa Limiter pasando la función como referencia.
-limiter = Limiter(
-    key_func=get_remote_address,
-    # Pasar la función sin parentesis (el callable)
-    default_limits=get_dynamic_limits
-)
-
-
-# # configurar rate limiter
+# # 1. Define la función que verifica la configuración.
+# def get_dynamic_limits():
+#     """Retorna límites normales o 0 si estamos en modo TESTING."""
+#     # current_app solo se llama cuando esta función se ejecuta en contexto.
+#     if current_app.config.get("testing", False):
+#         return ["0 per day"] # Deshabilita límites
+#     else:
+#         return ["200 per day", "50 per hour"]
+#
+#
+# # 2. Inicializa Limiter pasando la función como referencia.
 # limiter = Limiter(
 #     key_func=get_remote_address,
-#     default_limits=["200 per day", "50 per hour"]
-#     if not current_app.config.get("testing")
-#     else ["0 per day"]  # Deshabilitar límites en modo testing
-#     )
+#     # Pasar la función sin parentesis (el callable)
+#     default_limits=get_dynamic_limits
+# )
+
+
+# configurar rate limiter
+limiter = Limiter(
+    key_func=get_remote_address,
+    # default_limits=["200 per day", "50 per hour"]
+    # if not current_app.config.get("testing")
+    # else ["0 per day"]  # Deshabilitar límites en modo testing
+    )
 
